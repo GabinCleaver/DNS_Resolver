262bc2c52fe364def76c651d2c2468a75a552b5d
MrGabin <79531012+GabinCleaver@users.noreply.github.com>
2021-06-05 18:28:37 +0200

    Update dns_url.py

--- dns_url.py
+++ dns_url.py
@@ -1,4 +1,5 @@
 import dns.resolver
+import socket
 from colorama import Fore, init
 
 init()
@@ -7,17 +8,23 @@ dns_record = {}
 
 website = input(Fore.LIGHTBLUE_EX + "Entrez l'url de votre site: ")
 
-a_record = dns.resolver.resolve(website, 'A')
-for ipval in a_record:
-    dns_record[Fore.LIGHTYELLOW_EX + 'IP'] = ipval.to_text()
+try:
+    a_record = dns.resolver.resolve(website, 'A')
+    for ipval in a_record:
+        dns_record[Fore.LIGHTYELLOW_EX + 'IP'] = ipval.to_text()
 
-mx_record_list = []
+    mx_record_list = []
 
-mx_record = dns.resolver.resolve(website,'MX')
-for server in mx_record:
-   mx_record_list.append(server)
-for i, element in enumerate(mx_record_list):
-    dns_record['DNS', i+1] = element
+    mx_record = dns.resolver.resolve(website,'MX')
+    for server in mx_record:
+        mx_record_list.append(server)
+    for i, element in enumerate(mx_record_list):
+        dns_record['DNS', i+1] = element
 
-for key,value in dns_record.items():
-    print(Fore.LIGHTCYAN_EX +  f"{key} = {value}")
+    print (Fore.GREEN + f'Hostname: {website}')
+
+    for key,value in dns_record.items():
+        print(Fore.LIGHTCYAN_EX +  f"{key} = {value}")
+
+except socket.gaierror as error:
+    print (f"Invalide Hostname, l'erreur est {error}")    
